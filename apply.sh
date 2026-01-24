4fa6825efb4d85de92802edccdaa7a9884d1d5d8
adrien-gomar <gomar@users.noreply.github.com>
2026-01-24 22:13:44 +0100

    It works on Keynote, Numbers, Powerpoint

--- apply.sh
+++ apply.sh
@@ -4,37 +4,38 @@
 
 # Key encoding:
 # ^ = Control, @ = Command, ~ = Option, $ = Shift
-# All apps use: @^ order, leaf menu name, \U2190-\U2193 for arrows (←↑→↓)
-# \U21a9 = Return
+# @^ order, \U2190-\U2193 for arrows (←↑→↓), \U21a9 = Return
+# Full paths use \033 (escape) as separator to avoid conflicts
 
 # --- Figma ---
-defaults write com.figma.Desktop NSUserKeyEquivalents -dict-add 'Align Left' '@^\U2190'
-defaults write com.figma.Desktop NSUserKeyEquivalents -dict-add 'Align Right' '@^\U2192'
-defaults write com.figma.Desktop NSUserKeyEquivalents -dict-add 'Align Top' '@^\U2191'
-defaults write com.figma.Desktop NSUserKeyEquivalents -dict-add 'Align Bottom' '@^\U2193'
-defaults write com.figma.Desktop NSUserKeyEquivalents -dict-add 'Align Horizontal Centers' '@^\U21a9'
-defaults write com.figma.Desktop NSUserKeyEquivalents -dict-add 'Align Vertical Centers' '@^`'
+# Menu: Arrange > Align Left/Right/Top/Bottom/Horizontal Centers/Vertical Centers
+defaults write com.figma.Desktop NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Left' '@^\U2190'
+defaults write com.figma.Desktop NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Right' '@^\U2192'
+defaults write com.figma.Desktop NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Top' '@^\U2191'
+defaults write com.figma.Desktop NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Bottom' '@^\U2193'
+defaults write com.figma.Desktop NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Horizontal Centers' '@^\U21a9'
+defaults write com.figma.Desktop NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Vertical Centers' '@^`'
 
 # --- Keynote ---
-# Uses leaf menu name only, @^ order (like Numbers/Pages/PowerPoint)
-defaults write com.apple.iWork.Keynote NSUserKeyEquivalents -dict-add "Left" '@^\U2190'
-defaults write com.apple.iWork.Keynote NSUserKeyEquivalents -dict-add "Right" '@^\U2192'
-defaults write com.apple.iWork.Keynote NSUserKeyEquivalents -dict-add "Top" '@^\U2191'
-defaults write com.apple.iWork.Keynote NSUserKeyEquivalents -dict-add "Bottom" '@^\U2193'
-defaults write com.apple.iWork.Keynote NSUserKeyEquivalents -dict-add "Centre" '@^\U21a9'
-defaults write com.apple.iWork.Keynote NSUserKeyEquivalents -dict-add "Middle" '@^`'
+# Menu: Arrange > Align Objects > Left/Centre/Right/Top/Middle/Bottom
+defaults write com.apple.iWork.Keynote NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Objects\033Left' '@^\U2190'
+defaults write com.apple.iWork.Keynote NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Objects\033Right' '@^\U2192'
+defaults write com.apple.iWork.Keynote NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Objects\033Top' '@^\U2191'
+defaults write com.apple.iWork.Keynote NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Objects\033Bottom' '@^\U2193'
+defaults write com.apple.iWork.Keynote NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Objects\033Centre' '@^\U21a9'
+defaults write com.apple.iWork.Keynote NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Objects\033Middle' '@^`'
 
 # --- PowerPoint ---
-# Uses standard domain (not sandboxed container), leaf menu name only, @^ order
-defaults write com.microsoft.PowerPoint NSUserKeyEquivalents -dict-add "Align to Left" '@^\U2190'
-defaults write com.microsoft.PowerPoint NSUserKeyEquivalents -dict-add "Align to Right" '@^\U2192'
-defaults write com.microsoft.PowerPoint NSUserKeyEquivalents -dict-add "Align to Top" '@^\U2191'
-defaults write com.microsoft.PowerPoint NSUserKeyEquivalents -dict-add "Align to Bottom" '@^\U2193'
-defaults write com.microsoft.PowerPoint NSUserKeyEquivalents -dict-add "Align to Centre" '@^\U21a9'
-defaults write com.microsoft.PowerPoint NSUserKeyEquivalents -dict-add "Align to Middle" '@^`'
+# Menu: Arrange > Align or Distribute > Align to Left/Centre/Right/Top/Middle/Bottom
+defaults write com.microsoft.PowerPoint NSUserKeyEquivalents -dict-add $'\033Arrange\033Align or Distribute\033Align to Left' '@^\U2190'
+defaults write com.microsoft.PowerPoint NSUserKeyEquivalents -dict-add $'\033Arrange\033Align or Distribute\033Align to Right' '@^\U2192'
+defaults write com.microsoft.PowerPoint NSUserKeyEquivalents -dict-add $'\033Arrange\033Align or Distribute\033Align to Top' '@^\U2191'
+defaults write com.microsoft.PowerPoint NSUserKeyEquivalents -dict-add $'\033Arrange\033Align or Distribute\033Align to Bottom' '@^\U2193'
+defaults write com.microsoft.PowerPoint NSUserKeyEquivalents -dict-add $'\033Arrange\033Align or Distribute\033Align to Centre' '@^\U21a9'
+defaults write com.microsoft.PowerPoint NSUserKeyEquivalents -dict-add $'\033Arrange\033Align or Distribute\033Align to Middle' '@^`'
 
 # --- Numbers ---
-# Uses \033 (escape) as path separator for conflicting names
+# Menu: Arrange > Align Objects > Left/Centre/Right/Top/Middle/Bottom
 defaults write com.apple.iWork.Numbers NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Objects\033Left' '@^\U2190'
 defaults write com.apple.iWork.Numbers NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Objects\033Right' '@^\U2192'
 defaults write com.apple.iWork.Numbers NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Objects\033Top' '@^\U2191'
@@ -43,12 +44,12 @@ defaults write com.apple.iWork.Numbers NSUserKeyEquivalents -dict-add $'\033Arra
 defaults write com.apple.iWork.Numbers NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Objects\033Middle' '@^`'
 
 # --- Pages ---
-# Uses leaf menu name only, @^ order (like Numbers/PowerPoint)
-defaults write com.apple.iWork.Pages NSUserKeyEquivalents -dict-add "Left" '@^\U2190'
-defaults write com.apple.iWork.Pages NSUserKeyEquivalents -dict-add "Right" '@^\U2192'
-defaults write com.apple.iWork.Pages NSUserKeyEquivalents -dict-add "Top" '@^\U2191'
-defaults write com.apple.iWork.Pages NSUserKeyEquivalents -dict-add "Bottom" '@^\U2193'
-defaults write com.apple.iWork.Pages NSUserKeyEquivalents -dict-add "Centre" '@^\U21a9'
-defaults write com.apple.iWork.Pages NSUserKeyEquivalents -dict-add "Middle" '@^`'
+# Menu: Arrange > Align Objects > Left/Centre/Right/Top/Middle/Bottom
+defaults write com.apple.iWork.Pages NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Objects\033Left' '@^\U2190'
+defaults write com.apple.iWork.Pages NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Objects\033Right' '@^\U2192'
+defaults write com.apple.iWork.Pages NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Objects\033Top' '@^\U2191'
+defaults write com.apple.iWork.Pages NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Objects\033Bottom' '@^\U2193'
+defaults write com.apple.iWork.Pages NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Objects\033Centre' '@^\U21a9'
+defaults write com.apple.iWork.Pages NSUserKeyEquivalents -dict-add $'\033Arrange\033Align Objects\033Middle' '@^`'
 
 echo "Done. Restart apps for shortcuts to take effect."
